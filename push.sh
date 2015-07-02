6162f6419ee4e16a6170c011b3f2b7081a08c6cc
gnu4cn <unisko@gmail.com>
2015-07-02 20:05:38 +0800

    add files

---
+++ push.sh
@@ -0,0 +1,3 @@
+git add test.py string_service.py
+git commit -m "修改test.py"
+git push
--- string_service.py
+++ string_service.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 '''
 Created on 2015年6月24日
 
@@ -10,27 +10,28 @@ from tornado import httpserver, ioloop, options, web
 from apsw import textwrap
 
 options.define("port", default=8000, type=int, help=u"在给定的端口上运行")
+options.define("wrapWidth", default=80, type=int, help=u"wrap操作的默认宽度")
+
 
 class ReverseHandler(web.RequestHandler):
     def get(self, input):
         self.write(input[::-1] + "\n")
-        
+
+
 class WrapHandler(web.RequestHandler):
     def post(self):
-        text = self.get_argument('text')
-        width = self.get_argument('width', 40)
+        text = self.get_argument('text', "test sentence")
+        width = self.get_argument('width', options.options.wrapWidth)
         self.write(textwrap.fill(text, int(width)) + "\n")
-        
+
 if __name__ == "__main__":
     options.parse_command_line()
     app = web.Application(
-                          handlers=[
-                                    (r"/reverse/(\w+)", ReverseHandler),
-                                    (r"/wrap", WrapHandler)                                    
-                                    ]
-                          )
+        handlers=[
+            (r"/reverse/(\w+)", ReverseHandler),
+            (r"/wrap", WrapHandler)
+            ]
+        )
     http_server = httpserver.HTTPServer(app)
     http_server.listen(options.options.port)
     ioloop.IOLoop.instance().start()
-        
-        
